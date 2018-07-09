8a1abab1e86f2124d28ca7d4dfee0fae8397f81b
Kurkodejm <starfire24680@protonmail.com>
2018-07-09 20:15:42 +0000

    Small optimization

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,9 +1,6 @@
 function active(id) {
-	var list = document.getElementsByClassName("active");
-	for (var i = 0; i < list.length; i++) {
-	    // list[i] is a node with the desired class name
-		list[i].className="";
-	}
+	var a = document.getElementsByClassName("active");
+	a[0].className="";
   	document.getElementById(id).className = 'active';
 	document.getElementById('configuration').innerHTML= "<section class='wrapperconf'><p>LOLOLOLOLOL</p></section>";	
 }
