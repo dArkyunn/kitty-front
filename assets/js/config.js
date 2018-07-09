bdd5597e8dd390bcfc3523629387024de9444664
Kurkodejm <starfire24680@protonmail.com>
2018-07-09 20:20:46 +0000

    Pages in configuration

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,5 +2,20 @@ function active(id) {
 	var a = document.getElementsByClassName("active");
 	a[0].className="";
   	document.getElementById(id).className = 'active';
-	document.getElementById('configuration').innerHTML= "<section class='wrapperconf'><p>LOLOLOLOLOL</p></section>";	
+	if (id == 1){
+	document.getElementById('configuration').innerHTML= "<section class='wrapperconf'><p>Section 1</p></section>";	
+	}
+	if (id == 2){
+	document.getElementById('configuration').innerHTML= "<section class='wrapperconf'><p>Section 2</p></section>";	
+	}
+	if (id == 3){
+	document.getElementById('configuration').innerHTML= "<section class='wrapperconf'><p>Section 3</p></section>";	
+	}
+	if (id == 4){
+	document.getElementById('configuration').innerHTML= "<section class='wrapperconf'><p>Section 4</p></section>";	
+	}
+	if (id == 5){
+	document.getElementById('configuration').innerHTML= "<section class='wrapperconf'><p>Section 5</p></section>";	
+	}
+
 }
