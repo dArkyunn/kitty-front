818801592d3e968b246009e01ad9136c3b48c093
Kurkodejm <starfire24680@protonmail.com>
2018-07-12 23:55:29 +0000

    Add playground flashing button handler

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -1,7 +1,21 @@
-function handleclick(id){
-	var button=document.getElementById(id);
-	button.classList.toggle("pgalt");
-	button.classList.toggle("pg");
+function handleclicktoggle(id){
+	//var button=document.getElementById(id);
+	//button.classList.toggle("pgalt");
+	//button.classList.toggle("pg");
+	document.getElementById(id).classList.toggle("pgalt");
+	document.getElementById(id).classList.toggle("pg");
+}
+function handleclickflash(id){
+	//var button=document.getElementById(id);
+	//button.classList.toggle("pgalt");
+	//button.classList.toggle("pg");
+	document.getElementById(id).classList.toggle("pgalt");
+	document.getElementById(id).classList.toggle("pg");
+	setTimeout(function(){
+	document.getElementById(id).classList.toggle("pgalt");
+	document.getElementById(id).classList.toggle("pg");
+
+	}, 160);
 
 
 }
