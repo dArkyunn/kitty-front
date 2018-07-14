09b8ddcf006e1de8474aaa259a197dae38d8e8ad
Kurkodejm <starfire24680@protonmail.com>
2018-07-14 20:25:55 +0000

    Switch bugfix

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -9,8 +9,7 @@ function handleclickswitch(){
 	//var button=document.getElementById(id);
 	//button.classList.toggle("pgalt");
 	//button.classList.toggle("pg");
-	document.getElementById(arguments[0]).classList.toggle("pgalt");
-	document.getElementById(arguments[0]).classList.toggle("pg");
+	document.getElementById(arguments[0]).classList.add("pgalt");
         for (var i = 1; i < arguments.length; ++i){
             document.getElementById(arguments[i]).classList.remove("pgalt");
 	    document.getElementById(arguments[i]).classList.add("pg");
