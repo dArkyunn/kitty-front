84ea6793d03de02a69d8c16f000e49dbe3809c62
Kurkodejm <starfire24680@protonmail.com>
2018-07-20 01:38:11 +0000

    Page navigation functionality:

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -1,3 +1,4 @@
+	var maxpages = 20;
 function handleclicktoggle(id){
 	//var button=document.getElementById(id);
 	//button.classList.toggle("pgalt");
@@ -51,8 +52,93 @@ function handleclickflash(id){
 
 
 }
+function gotopage(id){
+	var a = document.getElementsByClassName("active");
+	a[0].className="";
+	if (id == "first"){
+		document.getElementById("1").innerHTML = "1"; 
+		document.getElementById("2").innerHTML = "2"; 
+		document.getElementById("3").innerHTML = "3";  
+		document.getElementById("4").innerHTML = "4"; 
+		document.getElementById("5").innerHTML = "5"; 
+		document.getElementById("6").innerHTML = "6"; 
+		document.getElementById("1").className = 'active';
+	}
+		if (id == "last"){
+		document.getElementById("1").innerHTML = +maxpages - 5; 
+		document.getElementById("2").innerHTML = +maxpages - 4; 
+		document.getElementById("3").innerHTML = +maxpages - 3;  
+		document.getElementById("4").innerHTML = +maxpages - 2; 
+		document.getElementById("5").innerHTML = +maxpages - 1; 
+		document.getElementById("6").innerHTML = +maxpages; 
+		document.getElementById("6").className = 'active';
+	}
+
+
+}
 function switchpage(id){
 	var a = document.getElementsByClassName("active");
 	a[0].className="";
-  	document.getElementById(id).className = 'active';
+	if(id == '5' && +document.getElementById("6").innerHTML < +maxpages){
+		document.getElementById("1").innerHTML =  +document.getElementById("1").innerHTML + 1 ; 
+		document.getElementById("2").innerHTML =  +document.getElementById("2").innerHTML + 1 ; 
+		document.getElementById("3").innerHTML =  +document.getElementById("3").innerHTML + 1 ; 
+		document.getElementById("4").innerHTML =  +document.getElementById("4").innerHTML + 1 ; 
+		document.getElementById("5").innerHTML =  +document.getElementById("5").innerHTML + 1 ; 
+		document.getElementById("6").innerHTML =  +document.getElementById("6").innerHTML + 1 ; 
+		document.getElementById(+id - 1).className = 'active';
+	}
+	else if(id == '6' && +document.getElementById("6").innerHTML < +maxpages - 1){	
+		document.getElementById("1").innerHTML =  +document.getElementById("1").innerHTML + 2 ; 
+		document.getElementById("2").innerHTML =  +document.getElementById("2").innerHTML + 2 ; 
+		document.getElementById("3").innerHTML =  +document.getElementById("3").innerHTML + 2 ; 
+		document.getElementById("4").innerHTML =  +document.getElementById("4").innerHTML + 2 ; 
+		document.getElementById("5").innerHTML =  +document.getElementById("5").innerHTML + 2 ; 
+		document.getElementById("6").innerHTML =  +document.getElementById("6").innerHTML + 2 ; 
+		document.getElementById(+id - 2).className = 'active';
+	}
+	else if(id == '6' && +document.getElementById("6").innerHTML < +maxpages){	
+		document.getElementById("1").innerHTML =  +document.getElementById("1").innerHTML + 1 ; 
+		document.getElementById("2").innerHTML =  +document.getElementById("2").innerHTML + 1 ; 
+		document.getElementById("3").innerHTML =  +document.getElementById("3").innerHTML + 1 ; 
+		document.getElementById("4").innerHTML =  +document.getElementById("4").innerHTML + 1 ; 
+		document.getElementById("5").innerHTML =  +document.getElementById("5").innerHTML + 1 ; 
+		document.getElementById("6").innerHTML =  +document.getElementById("6").innerHTML + 1 ; 
+		document.getElementById(+id - 1).className = 'active';
+	}
+
+	else if(id == '1' && +document.getElementById("1").innerHTML == "2" ){
+		document.getElementById("1").innerHTML =  +document.getElementById("1").innerHTML - 1 ; 
+		document.getElementById("2").innerHTML =  +document.getElementById("2").innerHTML - 1 ; 
+		document.getElementById("3").innerHTML =  +document.getElementById("3").innerHTML - 1 ; 
+		document.getElementById("4").innerHTML =  +document.getElementById("4").innerHTML - 1 ; 
+		document.getElementById("5").innerHTML =  +document.getElementById("5").innerHTML - 1 ; 
+		document.getElementById("6").innerHTML =  +document.getElementById("6").innerHTML - 1 ; 
+		document.getElementById(+id + 1).className = 'active';
+	}
+	else if(id == '1' && +document.getElementById("1").innerHTML > "2"){
+		document.getElementById("1").innerHTML =  +document.getElementById("1").innerHTML - 2 ; 
+		document.getElementById("2").innerHTML =  +document.getElementById("2").innerHTML - 2 ; 
+		document.getElementById("3").innerHTML =  +document.getElementById("3").innerHTML - 2 ; 
+		document.getElementById("4").innerHTML =  +document.getElementById("4").innerHTML - 2 ; 
+		document.getElementById("5").innerHTML =  +document.getElementById("5").innerHTML - 2 ; 
+		document.getElementById("6").innerHTML =  +document.getElementById("6").innerHTML - 2 ; 
+		document.getElementById(+id + 2).className = 'active';
+	}
+	else if(id == '2' && +document.getElementById("2").innerHTML > "2"){
+		document.getElementById("1").innerHTML =  +document.getElementById("1").innerHTML - 1 ; 
+		document.getElementById("2").innerHTML =  +document.getElementById("2").innerHTML - 1 ; 
+		document.getElementById("3").innerHTML =  +document.getElementById("3").innerHTML - 1 ; 
+		document.getElementById("4").innerHTML =  +document.getElementById("4").innerHTML - 1 ; 
+		document.getElementById("5").innerHTML =  +document.getElementById("5").innerHTML - 1 ; 
+		document.getElementById("6").innerHTML =  +document.getElementById("6").innerHTML - 1 ; 
+		document.getElementById(+id + 1).className = 'active';
+	}
+	else{
+		document.getElementById(id).className = 'active';
+
+
+	}
+
+
 }
