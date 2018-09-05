ec43a18c7e0e92f921c0e7ce2118978e0fb79961
Kurkodejm <starfire24680@protonmail.com>
2018-09-05 22:23:17 +0200

    Changes to playground, added some sliders

--- assets/js/playground.js
+++ assets/js/playground.js
@@ -54,6 +54,125 @@ function handleclickcolor(id){
 function handleclickflash(id){
 	socket.emit('sendaction', id);
 }
+function loadcontrols(){
+	slider();
+	roundslider();
+	roundquarterslider();
+	verticalslider();
+}
+function slider(){
+	var html = `<div class="control">
+ 			 <input type="range" min="1" max="100" value="50" class="slider" id="myRange">
+			</div>`
+	$("#controls").append(html);
+
+
+}
+function verticalslider(){
+	var html = `<div class="control">
+ 			 <input orient="vertical" style="height:160px;" type="range" min="1" max="100" value="50" class="slider" id="myRange">
+			</div>`
+	$("#controls").append(html);
+
+
+}
+
+function roundslider(){
+	var html =`<div class="control">
+                	<div id="type"></div>
+            	   </div>
+		<style>
+		#type .rs-range-color  {
+    background-color: #5673E7;
+}
+#type .rs-path-color  {
+    background-color: #dedede;
+}
+#type .rs-handle  {
+    background-color: #000000;
+}
+#type .rs-border  {
+    border-color: black;
+}
+#type .rs-bg-color  {
+    background-color: #333333;
+    opacity:1;
+}
+
+</style>`
+	$("#controls").append(html);
+
+            $("#type").roundSlider({
+                value: 45,
+		radius: 80,
+   		width: 14,
+  	        handleSize: "24,12",
+  	        handleShape: "square",
+ 	        sliderType: "min-range",
+   	        value: 40
+            });
+
+
+}
+function roundquarterslider(){
+	var html =`<div style="height:160px;width:270px;"class="control">
+                	<div id="type1"></div>
+            	   </div>
+		<style>
+		#type1 .rs-range-color  {
+    background-color: #5673E7;
+}
+#type1 .rs-path-color  {
+    background-color: #dedede;
+}
+#type1 .rs-handle  {
+    background-color: #000000;
+}
+#type1 .rs-border  {
+    border-color: black;
+}
+#type1 .rs-bg-color  {
+    background-color: #333333;
+    opacity:1;
+}
+#type1  {
+    height: 100% !important;
+    width: 100% !important;
+    overflow: hidden;
+    padding: 10px;
+}
+#type1 .rs-container  {
+    left: 50%;
+    margin-left: -180px;
+    height:360px !important;
+    width:360px !important;
+}
+#type1 .rs-tooltip  {
+    margin-top:-80px !important;
+}
+
+</style>`
+	$("#controls").append(html);
+
+            $("#type1").roundSlider({
+    		sliderType: "min-range",
+   		circleShape: "custom-quarter",
+   		min: 0,
+  		max: 11,
+    		value: 11,
+    		startAngle: 45,
+		editableTooltip: false,
+    		radius: 300,
+    		width: 20,
+    		handleShape: "dot",
+    		tooltipFormat: "tooltipVal1"
+             });
+
+
+}
+
+loadcontrols();
+
 function gotopage(id){
 	var a = document.getElementsByClassName("active");
 	a[0].className="";
@@ -81,6 +200,7 @@ function gotopage(id){
 function switchpage(id){
 	var a = document.getElementsByClassName("active");
 	a[0].className="";
+	var maxpages=20;
 	if(id == '5' && +document.getElementById("6").innerHTML < +maxpages){
 		document.getElementById("1").innerHTML =  +document.getElementById("1").innerHTML + 1 ; 
 		document.getElementById("2").innerHTML =  +document.getElementById("2").innerHTML + 1 ; 
